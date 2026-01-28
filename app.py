abc549223203aaed47574fb1b0ba373b0eea6505
Luyolo-Vacu <1106467@students.wits.ac.za>
2026-01-28 14:24:35 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 17:32:56 2026
+
+@author: BBarsch
+"""
+
+import streamlit as st
+
+st.write("Hello:)")
+
+st.title("My first streamlit app")
+
+st.write("CSS26")
+
+st.write("Are you winning?")
+
+st.slider("pick a number",1,100)
